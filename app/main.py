002b33368136cd529c125d8c1f28e355f29445ac
bilalron <bilal.ron@berkeley.edu>
2025-02-02 14:38:22 -0800

    Fixed Cors

--- app/main.py
+++ app/main.py
@@ -21,7 +21,6 @@ app = FastAPI(title="Document QA API 1")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
